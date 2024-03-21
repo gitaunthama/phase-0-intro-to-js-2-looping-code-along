ac82c1a88f5fb05e4a7a1e74798e3cacb958ed64
gitaunthama <gitaunthama@gmail.com>
2024-03-21 23:22:03 +0300

    complete work

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names= [ "Guadalupe","Ollie" ,"Aki"];
+const message= [ "surprise"];
+function writeCards(name, message) {
+  return name.map(name => `Thank you, ${name}, for the wonderful ${message} gift!`)
+}
+console.log(writeCards(names, "gift"));
+
+function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  countDown();
+  
